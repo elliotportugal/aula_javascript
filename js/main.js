99ec4d05b58137ca0c7ee026c63ea8db15f7e4ea
elliot.silva <elliotportugal@gmail.com>
2021-01-23 01:02:11 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var nome = "Elliot Portugal Silva"
 var idade = 31;
 var idade2 = 10;
-//console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
-//console.log(idade+idade2);
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+console.log(idade+idade2);
