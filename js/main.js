33379ea10b183714a8d87a94c2025a69df31e0ee
elliot.silva <elliotportugal@gmail.com>
2021-01-23 21:59:39 -0300

    Curso 1 - Finalizado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,117 @@
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+        //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("http://www.google.com.br");
+    //window.location.href = "http://google.com.br";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "<i>Parabéns!</i>";
+    //document.getElementById("mousemove").innerHTML = "<i>Parabéns!</i>";
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui.";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui.";
+ }
+
+ function load(){
+    alert("Página carregada.")
+ }
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+*/
+ /*
+function soma(n1,n2){
+    return n1+n2;
+}
+console.log(soma(19,21));
+*/
+/*
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace("Laura é linda.", "Laura", "Ingrid"));
+*/
+
+/*function validaIdade(idade){
+    var validar
+
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
+var d = new Date();
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getMonth()+1);
+console.log(d.getUTCDay());
+*/
+
+/*
+var count;
+for (count=0; count<=5; count++ ){
+    console.log(count);
+};
+*/
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade>=18){
+    console.log("maior idade");
+}else{
+    console.log("menor");
+}
+*/
+
+
+/*
+var frutas = [{nome:"Maçã", cor:"Vermelho"}, {nome:"Uva", cor:"Roxo"}];
+console.log(frutas);
+*/
+
+/*var fruta = {nome:"maça", cor:"vermelho"};
+console.log(fruta);
+*/
+
+/*
+var lista = ["Elliot", "Ingrid", "Laura"];
+lista.push("Rafael");
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));
+*/
+
+
+/*
 var nome = "Elliot Portugal Silva"
 var idade = 31;
 var idade2 = 10;
 console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
 console.log(idade+idade2);
+*/
